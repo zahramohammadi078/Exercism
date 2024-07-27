d4bdc1fa7b07f40e95d12f2c93e77473e6902303
zahra <zahramohamadi257@gmail.com>
2024-07-27 16:24:46 +0330

    Sum-of-Multiples

---
+++ Sum-of-Multiples.js
@@ -0,0 +1,19 @@
+  export const sum = function(MNumArray, num) {
+    let multiples = [];
+  
+   
+    for (let factor of MNumArray) {
+      if (factor === 0) continue;
+      for (let i = factor; i < num; i += factor) {
+        if (multiples.indexOf(i) === -1) {
+          multiples.push(i);
+        }
+      }
+    }
+  
+   
+    return multiples.reduce(function(acc, val) {
+      return acc + val;
+    }, 0);
+  };
+  
